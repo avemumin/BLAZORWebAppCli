4ff100fb419ba210a38ec36c6a2b4dd4d7acee0d
Jacek Szczepański <jacogitproject@gmail.com>
2020-05-31 21:21:43 +0200

    31.05.2020 changes with controller and mail

--- SGNWebAppCli/Data/QualityDetail.cs
+++ SGNWebAppCli/Data/QualityDetail.cs
@@ -9,5 +9,6 @@
         public string QualityValue { get; set; }
         public string Symbol { get; set; }
         public string ModeValue { get; set; }
+        public short IdCurrency { get; set; }
     }
 }
